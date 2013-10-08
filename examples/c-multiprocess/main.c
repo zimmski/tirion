75b9455975ea87b875a94e05e401ae6b9bd5d097
Markus Zimmermann <markus.zimmermann@nethead.at>
2013-10-08 23:35:10 +0200

    write into the memory to actually use it

--- examples/c-multiprocess/main.c
+++ examples/c-multiprocess/main.c
@@ -2,6 +2,7 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -111,6 +112,9 @@ int main (int argc, char **argv) {
 				int j = 0;
 				for (; j < flagMbPerChild; j++) {
 					a[j] = malloc(aSize);
+
+					// fill the memory with something
+					memset (a[j], '1', aSize);
 
 					tirionV(tirion, "Child %d allocated another MB", i);
 					tirionInc(tirion, MetricAllocated);
