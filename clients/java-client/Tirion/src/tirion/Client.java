7860aca82688d096da34e976031f5e6798a16b34
Markus Zimmermann <markus.zimmermann@nethead.at>
2013-11-04 15:18:53 +0100

    rest of Client library

--- clients/java-client/Tirion/src/tirion/Client.java
+++ clients/java-client/Tirion/src/tirion/Client.java
@@ -1,134 +1,98 @@
 package tirion;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.RandomAccessFile;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.concurrent.locks.*;
+import java.util.concurrent.locks.Lock;
+
+import com.etsy.net.*;
 
 public class Client {
 	public static String TIRION_VERSION = "0.2";
-	
+
 	private static int floatSize = 4;
 	private static String logPrefix = "[client]";
 	private static int tirionTagSize = 513;
-	
+
 	private int count;
+	private Thread handleCommands;
 	private FloatBuffer metrics;
 	private Lock metricLock;
+	private UnixDomainSocketClient net;
+	private BufferedReader netIn;
+	private OutputStream netOut;
 	private boolean running;
 	private String socket;
 	private boolean verbose;
-	
+
 	public Client(String socket, boolean verbose) {
 		this.count = 0;
+		this.handleCommands = null;
 		this.metrics = null;
+		this.net = null;
+		this.netIn = null;
+		this.netOut = null;
 		this.running = false;
 		this.socket = socket;
 		this.verbose = verbose;
 	}
-	
+
 	// Init initializes the client
-	public void init() {
-		var err error
+	public void init() throws Exception {
+		/*
+		 * TODO find out how to do this in java... if r, err :=
+		 * syscall.Setsid(); r == -1 {
+		 * this.e("Cannot set new session and group id of process: %v");
+		 * 
+		 * return err }
+		 */
 
-		if r, err := syscall.Setsid(); r == -1 {
-			this.e("Cannot set new session and group id of process: %v");
+		this.v("Open unix socket to %s", this.socket);
+		this.net = new UnixDomainSocketClient(this.socket, JUDS.SOCK_STREAM);
+		this.netIn = new BufferedReader(new InputStreamReader(this.net.getInputStream()));
+		this.netOut = this.net.getOutputStream();
 
-			return err
+		this.v("Request tirion protocol version v%s", tirion.Client.TIRION_VERSION);
+		this.send("tirion v" + tirion.Client.TIRION_VERSION + "\tmmap");
+
+		String[] t = this.receive().split("\t");
+
+		if (t.length < 2 || t[1].length() == 0) {
+			throw new Exception("Did not receive correct metric count and protocol URL");
 		}
 
-		this.v("Open unix socket to %s", c.socket)
-		c.fd, err = net.Dial("unix", c.socket)
+		try {
+			this.count = Integer.parseInt(t[0]);
+		} catch (NumberFormatException e) {
+			this.e("Did not receive correct metric count");
 
-		if err != nil {
-			if strings.HasSuffix(err.Error(), "use of closed network connection") || strings.HasSuffix(err.Error(), "no such file or directory") {
-				this.e("Cannot open unix socket %s", c.socket);
-			}
-
-			return err
-		} else {
-			this.v("Request tirion protocol version v%s", Version)
-			if err := c.send("tirion v" + Version + "\t" + c.PreferredMetricProtocoll); err != nil {
-				this.e(err.Error())
-
-				return err
-			}
-
-			m, err := c.receive()
-
-			switch err {
-			case nil:
-				var t = strings.SplitN(m, "\t", 2)
-
-				if len(t) == 1 || t[1] == "" {
-					err := errors.New("Did not receive correct metric count and protocol URL");
-
-					this.e(err.Error())
-
-					return err
-				}
-
-				var metricCount, err = strconv.Atoi(t[0])
-
-				if err != nil {
-					this.e("Did not receive correct metric count");
-
-					return err
-				}
-
-				u, err := url.Parse(t[1])
-
-				if err != nil {
-					this.e("Did not receive correct protocol URL");
-
-					return err
-				}
-
-				this.v("Received metric count %d and protocol URL %v", metricCount, u)
-
-				c.metricsCollector, err = collector.NewCollector(u.Scheme)
-
-				if err != nil {
-					this.e("Cannot create metric collector");
-
-					return err
-				}
-				
-				if _, err := os.Stat(u.Path); os.IsNotExist(err) {
-					return errors.New(fmt.Sprintf("Cannot open mmap file: %v", err))
-				}
-
-				err = c.metricsCollector.InitClient(u, int32(metricCount))
-
-				if err != nil {
-					this.e("Cannot initialize metrics collector");
-
-					return err
-				}
-
-				this.v("Initialized metric collector %s", u.Scheme);
-
-				this.running = true;
-
-				// we want to handle commands not in the main thread
-				go c.handleCommands()
-			case io.EOF:
-				this.v("Unix socket got closed with EOF");
-
-				return err
-			default:
-				if strings.HasSuffix(err.Error(), "use of closed network connection") {
-					this.v("Unix socket suddenly got closed");
-				}
-
-				return err
-			}
+			throw e;
 		}
 
-		return nil
+		if (!t[1].startsWith("mmap://")) {
+			throw new Exception("Did not receive correct protocol URL");
+		}
+
+		String u = t[1].substring(7);
+
+		this.v("Received metric count %d and protocol URL %s", this.count, u);
+
+		this.mmapOpen(u);
+
+		this.v("Initialized metric collector mmap");
+
+		this.running = true;
+
+		// we want to handle commands not in the main thread
+		this.handleCommands = new Thread(new HandleCommands());
+		this.handleCommands.start();
 	}
 
 	// Close uninitializes the client by closing all connections of the client.
@@ -137,12 +101,22 @@ public class Client {
 
 		this.mmapClose();
 
-		if c.fd != nil {
-			if err := c.fd.Close(); err != nil {
-				return err
-			}
+		if (this.net != null) {
+			this.net.close();
 
-			c.fd = nil
+			this.netIn = null;
+			this.netOut = null;
+			this.net = null;
+		}
+
+		if (this.handleCommands != null) {
+			try {
+				this.handleCommands.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			this.handleCommands = null;
 		}
 	}
 
@@ -155,15 +129,15 @@ public class Client {
 		if (i < 0 || i >= this.count) {
 			return 0.0f;
 		}
-		
+
 		float f = 0.0f;
-		
+
 		this.metricLock.lock();
-		
+
 		try {
 			if (this.metrics != null) {
 				f = this.metrics.get(i) + v;
-				
+
 				this.metrics.put(i, f);
 			}
 		} finally {
@@ -190,13 +164,13 @@ public class Client {
 		}
 
 		float f = 0.0f;
-		
+
 		this.metricLock.lock();
-		
+
 		try {
 			if (this.metrics != null) {
 				f = this.metrics.get(i) - v;
-				
+
 				this.metrics.put(i, f);
 			}
 		} finally {
@@ -205,23 +179,24 @@ public class Client {
 
 		return f;
 	}
-	
+
 	public boolean running() {
 		return this.running;
 	}
 
 	// Tag sends a tag to the agent
-	public void tag(String format, Object... args) {
+	public void tag(String format, Object... args) throws IOException {
 		this.send(this.prepareTag(String.format("t" + format, args)));
 	}
-	
+
 	// D outputs a Tirion debug message.
 	public void d(String format, Object... args) {
 		if (!this.verbose) {
 			return;
 		}
-		
-		System.err.print(this.logPrefix + "[debug] " + String.format(format, args) + "\n");
+
+		System.err.print(Client.logPrefix + "[debug] "
+				+ String.format(format, args) + "\n");
 	}
 
 	// E outputs a Tirion error message.
@@ -229,8 +204,9 @@ public class Client {
 		if (!this.verbose) {
 			return;
 		}
-		
-		System.err.print(this.logPrefix + "[error] " + String.format(format, args) + "\n");
+
+		System.err.print(Client.logPrefix + "[error] "
+				+ String.format(format, args) + "\n");
 	}
 
 	// V outputs a Tirion verbose message.
@@ -238,64 +214,29 @@ public class Client {
 		if (!this.verbose) {
 			return;
 		}
-		
-		System.err.print(this.logPrefix + "[verbose] " + String.format(format, args) + "\n");
+
+		System.err.print(Client.logPrefix + "[verbose] "
+				+ String.format(format, args) + "\n");
 	}
 
-	private void handleCommands() {
-		this.v("Start listening to commands");
-	
-		for c.Running {
-			var data, err = c.receive()
-	
-			switch err {
-			case nil:
-				com := data[0]
-	
-				switch com {
-				default:
-					this.e("Unknown command '%c'", com)
-				}
-			case io.EOF:
-				this.v("Unix socket got closed with EOF")
-	
-				c.Running = false
-			default:
-				if strings.HasSuffix(err.Error(), "use of closed network connection") {
-					if c.Running {
-						this.v("Unix socket suddenly got closed")
-					}
-				} else {
-					this.e("%v", err)
-				}
-	
-				c.Running = false
-	
-				break
-			}
-		}
-	
-		this.v("Stop listening to commands")
-	}
-	
-	private void mmapOpen(String filename, int metricCount) throws IOException {
+	private void mmapOpen(String filename) throws IOException {
 		RandomAccessFile file = new RandomAccessFile(filename, "rw");
-        MappedByteBuffer buffer = file.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, floatSize * metricCount);
-        
-        buffer.limit(floatSize * metricCount);
-        buffer.order(ByteOrder.LITTLE_ENDIAN);
-        
-        //buffer.force();
-        buffer.load();
+		MappedByteBuffer buffer = file.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, floatSize * this.count);
 
-        this.metrics = buffer.asFloatBuffer();
+		buffer.limit(floatSize * this.count);
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
 
-        file.close();
+		// buffer.force();
+		buffer.load();
+
+		this.metrics = buffer.asFloatBuffer();
+
+		file.close();
 	}
-	
+
 	private void mmapClose() {
 		this.metricLock.lock();
-		
+
 		try {
 			this.metrics = null;
 		} finally {
@@ -310,22 +251,45 @@ public class Client {
 
 		return tag.replace("\n", " ");
 	}
-	
-	private String receive() {
-		var buf = make([]byte, 4096)
 
-		nr, err := t.fd.Read(buf)
-
-		if err != nil {
-			return "", err
-		}
-
-		return strings.Trim(string(buf[0:nr]), "\n"), nil
+	private String receive() throws IOException {
+		return this.netIn.readLine();
 	}
 
-	private void send(String msg) {
-		_, err := t.fd.Write([]byte(msg + "\n"))
+	private void send(String msg) throws IOException {
+		this.netOut.write((msg + "\n").getBytes());
+	}
 
-		return err
+	private class HandleCommands implements Runnable {
+		@Override
+		public void run() {
+			Client.this.v("Start listening to commands");
+
+			while (Client.this.running) {
+				Exception e = null;
+				String s = null;
+
+				try {
+					s = Client.this.receive();
+				} catch (Exception t) {
+					e = t;
+				}
+
+				if (e == null) {
+					char com = s.charAt(0);
+
+					switch (com) {
+					default:
+						Client.this.e("Unknown command '%c'", com);
+					}
+				} else {
+					Client.this.e("Unix socket error: " + e);
+
+					Client.this.running = false;
+				}
+			}
+
+			Client.this.v("Stop listening to commands");
+		}
 	}
 }
