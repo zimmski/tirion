d5a6a663fd37dd68019c4fdadd553e129abd9588
Markus Zimmermann <markus.zimmermann@nethead.at>
2014-11-06 13:05:20 +0100

    go sub-repositories have moved

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for FOLDER in code.google.com/p/go.net/websocket github.com/howeyc/fsnotify github.com/lib/pq github.com/robfig/config github.com/streadway/simpleuuid github.com/robfig/pathtree github.com/robfig/revel github.com/robfig/revel/revel; do
+for FOLDER in golang.org/x/net/websocket github.com/howeyc/fsnotify github.com/lib/pq github.com/robfig/config github.com/streadway/simpleuuid github.com/robfig/pathtree github.com/robfig/revel github.com/robfig/revel/revel; do
 	echo "Process $FOLDER"
 	rm -rf $GOPATH/pkg/*/$FOLDER*
 	if [ -d "$GOPATH/src/$FOLDER" ]; then
